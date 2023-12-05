ebc483cde9a746f998691f0c528492fde431a9f0
Nuno Almeida <69082781+nuno-almeida@users.noreply.github.com>
2023-12-05 19:03:01 +0000

    added bootstrap 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
